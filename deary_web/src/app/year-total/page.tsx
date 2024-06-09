45fc330d017d3b9d839d9d0811b8afea70cc51ba
Tunyakon <junnessc47@gmail.com>
2024-06-09 14:57:34 +0700

    responsive

--- deary_web/src/app/year-total/page.tsx
+++ deary_web/src/app/year-total/page.tsx
@@ -228,17 +228,19 @@ export default function Page() {
     width: "200px",
     height: "200px",
     borderRadius: "50%",
-    background: `radial-gradient(circle, ${gradientString})`,
+    background: `linear-gradient(43deg, ${gradientString})`,
+    boxShadow:
+      "inset 0 0 10px rgba(255, 255, 255, 1), inset 0 0 20px rgba(255, 255, 255, 1)",
     position: "relative",
     overflow: "hidden",
-    boxShadow: "0 0 30px 0px rgba(255, 255, 255, 0.5) inset",
+    // boxShadow: "0 0 30px 0px rgba(255, 255, 255, 0.5) inset",
   };
 
   return (
     <main className="flex flex-col w-screen min-h-screen bg-[url('/image/gridbg.png')] bg-repeat font-mitr text-[#212121]">
       <div className="w-screen flex justify-center">
         <div className="flex flex-col items-center mt-10">
-          <h1 className="text-[50px] font-semibold">All Your Memory</h1>
+          <h1 className="text-[50px] font-semibold mb-5">All Your Memory</h1>
           {pages.length === 0 ? (
             <div className="flex justify-center items-center h-full mt-10">
               <h2 className="text-[22px] font-medium opacity-50 mt-20">
@@ -255,28 +257,45 @@ export default function Page() {
                     width: "45%",
                     height: "25%",
                     top: "10%",
-                    left: "30%",
+                    left: "27%",
                     background:
                       "linear-gradient(to bottom, rgba(255, 255, 255, 0.6), transparent)",
                     zIndex: "2",
                     borderRadius: "50%",
                   }}
                 ></div>
+                  <div
+                    style={{
+                      position: "absolute",
+                      bottom: "0",
+                      left: "85px",
+                      width: "28px",
+                      height: "13px",
+                      borderRadius: "50%",
+                      boxShadow: `inset 0 0 10px rgba(255, 255, 255, 0.5),
+                                  inset 0 0 20px rgba(255, 255, 255, 1),
+                                  0 0 30px rgba(255, 255, 255, 1),
+                                  0 0 40px rgba(255, 255, 255, 1),
+                                  0 0 50px rgba(255, 255, 255, 1)`,
+                      filter: "blur(3px)"
+                    }}
+                  ></div>
+
               </div>
-              <div className="w-full h-10 text-center mt-6 text-3xl font-medium">
+              <div className="w-full h-10 text-center mt-6 text-3xl font-medium mb-5">
                 {" "}
                 This Orb is all your mood!!
               </div>
 
-              <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-7">
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-5 mt-7">
                 {pages.map((page, index) => (
                   <div
                     key={index}
-                    className="flex flex-col w-[450px] h-auto bg-[#FFFFFF] bg-opacity-[20%] backdrop-blur-sm p-10 rounded-[50px] shadow-[0_3px_10px_rgb(0,0,0,0.2)] relative"
+                    className="flex flex-col w-[300px] lg:w-[350px] xl:w-[450px] h-auto bg-[#FFFFFF] bg-opacity-[20%] backdrop-blur-sm p-10 rounded-[50px] shadow-[0_3px_10px_rgb(0,0,0,0.2)] relative"
                   >
                     <div className="flex flex-col justify-center ">
                       <div className="flex flex-row justify-between">
-                        <h1 className="text-[25px] font-medium">
+                        <h1 className="text-[18px] sm:text-[20px] lg:text-[25px] font-medium">
                           {page.dateandtime}
                         </h1>
                         <div className="flex justify-end">
@@ -284,7 +303,7 @@ export default function Page() {
                             onClick={() => toggleDropdown(index)}
                             className="relative"
                           >
-                            <span className="text-[24px] opacity-50">x</span>
+                            <span className="text-[18px] sm:text-[20px] lg:text-[24px] opacity-50">x</span>
                           </button>
                           {dropdownOpen === index && (
                             <div
@@ -407,7 +426,7 @@ export default function Page() {
                         )}
                       </div>
                     </div>
-                    <div className="text-[22px] mt-[10px] whitespace-pre-wrap break-words overflow-hidden relative">
+                    <div className="text-[18px] sm:text-[20px] lg:text-[22px] mt-[10px] whitespace-pre-wrap break-words overflow-hidden relative">
                       <p className="line-clamp-4 text-center">{page.text}</p>
                       {shouldShowReadMore(page.text) && (
                         <div className="flex justify-end px-3">
